5403bc42e9b50c3a4137e91416bcb28bbd7e4ddd
Hung Cao <hungqcao@Hungs-MacBook-Pro.local>
2015-06-13 16:05:07 -0500

    Initialize

---
+++ tools.js
@@ -0,0 +1,24 @@
+module.exports = {
+  copyData : function name(source, params) {
+	if(params){
+		for(var prop in params){
+			if(params.hasOwnProperty(prop)){
+				source[prop] = params[prop];
+			}
+		}
+	}else{
+		throw "Object is null!";
+	}
+  },
+  encryptData : function name(source, key) {
+	if(key){
+		for(var prop in source){
+			if(source.hasOwnProperty(prop)){
+				source[prop] = key.encrypt(source[prop], 'utf8', 'base64');
+			}
+		}
+	}else{
+		throw "Key is null!";
+	}
+  }
+};
